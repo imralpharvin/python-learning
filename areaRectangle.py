7e4d9bf4dff8fafc3c7b8df22c598a62ff8b4aea
Ralph Arvin De Castro <ralpharvin.dc@gmail.com>
2019-12-26 11:35:16 -0500

    added documentation and area of rectangle script

---
+++ areaRectangle.py
@@ -0,0 +1,9 @@
+# Name: Ralph Arvin De Castro
+# Date: December 26, 2019
+# Program: This program determines the area of a rectangle
+
+width = 9
+height = 9 * 9
+area = width * height
+
+print('The area of the rectangle is ' + str(area))
---
+++ helloWorld.py
@@ -0,0 +1,5 @@
+# Name: Ralph Arvin De Castro
+# Date: December 24, 2019
+# Program: This program prints Hello World!
+
+print ("Hello World!")
